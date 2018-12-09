f5c08e39a67ffc8997b57a9272d1ead7b0e0902f
Serje <shepelevich.serje@gmail.com>
2018-12-09 17:23:05 +0300

    started implementations

---
+++ DIContainer/DIContainer/Implementations/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using DIContainer.Interfaces;
+
+namespace DIContainer.Implementations
+{
+    public class Configuration : IConfiguration
+    {
+        public void Register<TImplementation>() where TImplementation : class
+        {
+            //
+        }
+        public void Register<TDependency, TImplementation>()
+           where TDependency : class
+           where TImplementation : TDependency
+        {
+            //
+        }
+
+    }
+}
---
+++ DIContainer/DIContainer/Implementations/Container.cs
@@ -0,0 +1,19 @@
+﻿using DIContainer.Interfaces;
+
+namespace DIContainer.Implementations
+{
+    public class Container : IContainer
+    {
+        private readonly IConfiguration configuration;
+
+        public Container(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public TDependency Resolve<TDependency>() where TDependency : class
+        {
+            return null;
+        }
+    }
+}
